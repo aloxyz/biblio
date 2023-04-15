413497dfb8c4a286eff5ac1f8ef671cac88436bb
aloxyz <aloisiogabriele@gmail.com>
2023-04-15 17:48:56 +0200

    navbar toggle

--- components/Button.js
+++ components/Button.js
@@ -1,6 +1,6 @@
 export default function Button(props) {
     return (
-        <button className='transition duration-150 hover:brightness-125 p-2 bg-emerald-800 rounded-md text-white'>
+        <button className='btn'>
             {props.children}
         </button>
     )
--- components/Navbar.js
+++ components/Navbar.js
@@ -1,39 +1,55 @@
-import {BookmarkSlashIcon, BookOpenIcon, Cog6ToothIcon, UserCircleIcon} from '@heroicons/react/24/outline'
+import { BookmarkSlashIcon, BookOpenIcon, Cog6ToothIcon, UserCircleIcon, XMarkIcon, Bars3Icon } from '@heroicons/react/24/outline'
 import Link from 'next/link'
+import Button from './Button';
+import { useState } from 'react';
 
 export default function Navbar() {
+
+    const [showSidebar, setShowSidebar] = useState(false);
+
     return (
-        <nav className='top-0 right-0 w-[50vw] bg-emerald-800 p-10 text-white fixed h-full flex flex-col items-center gap-5'>
-        
-        <Link href='/user' className='flex flex-col items-center hover:underline'>
-            <UserCircleIcon className='w-14 stroke-1'/>
-            Mario Rossi
-        </Link>
-        
-        
-        <ul>
-            <li className='hover:underline'>
-                <Link href='/books'>
-                <BookOpenIcon className='w-5 pb-1 inline'/>
-                Books
+        <>
+            {
+                showSidebar ? (
+                    <XMarkIcon onClick={() => setShowSidebar(!showSidebar)}/>
+                ) : (
+                    <Bars3Icon onClick={() => setShowSidebar(!showSidebar)}/>
+                )
+            }
+
+            <nav className={`${showSidebar ? "translate-x-0 " : "translate-x-full"} top-0 right-0 w-[50vw] bg-emerald-800 p-10 text-white fixed h-full flex flex-col items-center gap-5`}>
+
+                <Link href='/user' className='flex flex-col items-center hover:underline'>
+                    <UserCircleIcon className='w-14 stroke-1' />
+                    Mario Rossi
                 </Link>
-            </li>
-            
-            <li className='hover:underline'>
-                <Link href='/readlist'>
-                <BookmarkSlashIcon className='w-5 pb-1 inline'/>
-                My readlist
-                </Link>
-            </li>
-    
-            <li className='hover:underline'>
-                <Link href='/settings'>
-                <Cog6ToothIcon className='w-5 pb-1 inline'/>
-                Settings
-                </Link>
-                </li>
-        </ul>
-    </nav>
+
+
+                <ul>
+                    <li className='hover:underline'>
+                        <Link href='/books'>
+                            <BookOpenIcon className='w-5 pb-1 inline' />
+                            Books
+                        </Link>
+                    </li>
+
+                    <li className='hover:underline'>
+                        <Link href='/readlist'>
+                            <BookmarkSlashIcon className='w-5 pb-1 inline' />
+                            My readlist
+                        </Link>
+                    </li>
+
+                    <li className='hover:underline'>
+                        <Link href='/settings'>
+                            <Cog6ToothIcon className='w-5 pb-1 inline' />
+                            Settings
+                        </Link>
+                    </li>
+                </ul>
+            </nav>
+        </>
+
     )
 
 }
