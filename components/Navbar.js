53094e291e45a9cacaec8e43cdded02855d771bb
aloxyz <aloisiogabriele@gmail.com>
2023-04-15 17:53:50 +0200

    navbar toggle icons

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,6 +1,5 @@
 import { BookmarkSlashIcon, BookOpenIcon, Cog6ToothIcon, UserCircleIcon, XMarkIcon, Bars3Icon } from '@heroicons/react/24/outline'
 import Link from 'next/link'
-import Button from './Button';
 import { useState } from 'react';
 
 export default function Navbar() {
@@ -11,13 +10,13 @@ export default function Navbar() {
         <>
             {
                 showSidebar ? (
-                    <XMarkIcon onClick={() => setShowSidebar(!showSidebar)}/>
+                    <XMarkIcon onClick={() => setShowSidebar(!showSidebar)} className='stroke-1 stroke-white w-12 cursor-pointer fixed right-0 top-0 z-50'/>
                 ) : (
-                    <Bars3Icon onClick={() => setShowSidebar(!showSidebar)}/>
+                    <Bars3Icon onClick={() => setShowSidebar(!showSidebar)} className='stroke-1 stroke-emerald-800 w-12 cursor-pointer fixed right-0 top-0 z-50'/>
                 )
             }
 
-            <nav className={`${showSidebar ? "translate-x-0 " : "translate-x-full"} top-0 right-0 w-[50vw] bg-emerald-800 p-10 text-white fixed h-full flex flex-col items-center gap-5`}>
+            <nav className={`${showSidebar ? "translate-x-0 " : "translate-x-full"} top-0 right-0 w-[50vw] bg-emerald-800 p-10 text-white fixed h-full flex flex-col items-center gap-5 ease-in-out duration-300`}>
 
                 <Link href='/user' className='flex flex-col items-center hover:underline'>
                     <UserCircleIcon className='w-14 stroke-1' />
