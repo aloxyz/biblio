216a5f7744418d4f80934c823288dd5036e6b51a
aloxyz <aloisiogabriele@gmail.com>
2023-04-16 16:03:05 +0200

    login and link styling

--- pages/login.js
+++ pages/login.js
@@ -1,25 +1,21 @@
 import Link from "next/link"
 
-export default function signup() {
+export default function login() {
     return (
         <form action="" className="flex flex-grow flex-col gap-3 text-center">
-            <h1>Create an account</h1>
+            <h1>Sign in</h1>
             <input type="text" name="username" id="username" placeholder="Username"/>
             <input type="password" name="password" id="password" placeholder="Password"/>
-            <input type="password" name="confirm-password" id="confirm-password" placeholder="Confirm password"/>
-
             
             <div className="space-x-4 self-start">
                 <input name="terms" type="checkbox"/>
-                <label htmlFor="terms">I accept the terms and conditions</label>
+                <label htmlFor="remember">Remember me</label>
             </div>
-
-            
-            <button className="btn" type="submit">Sign up</button>
+            <button className="btn" type="submit">Sign in</button>
 
             <div>
-            <p>You already have an account?</p>
-            <Link href="login">Click here to sign in</Link>
+            <p>You do not have an account?</p>
+            <Link href="signup">Click here to create one</Link>
             </div>
 
         </form>
