f2af570b186808198d773aa84bfa6dfe70d80498
Gabriele Aloisio <aloisiogabriele@gmail.com>
2023-04-30 14:15:31 +0200

    new db schema

---
+++ backend/schema.sql
@@ -0,0 +1,29 @@
+-- Adminer 4.8.1 MySQL 5.5.5-10.11.2-MariaDB-1:10.11.2+maria~ubu2204 dump
+
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
+
+USE `biblio`;
+
+SET NAMES utf8mb4;
+
+CREATE TABLE `readlists` (
+  `user_id` int(10) unsigned NOT NULL,
+  `book_olid` varchar(128) NOT NULL,
+  PRIMARY KEY (`user_id`),
+  CONSTRAINT `readlists_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`) ON DELETE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+CREATE TABLE `users` (
+  `id` int(10) unsigned NOT NULL AUTO_INCREMENT,
+  `email` varchar(64) NOT NULL,
+  `password` varchar(64) NOT NULL,
+  `name` varchar(128) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+
+-- 2023-04-30 12:14:50
