3d6ff5b13061f640ba9e85a098020dc84fb712ed
Gabriele Aloisio <aloisiogabriele@gmail.com>
2023-04-30 16:15:18 +0200

    api OL layer

--- backend/fetch.php
+++ backend/fetch.php
@@ -1,9 +1,19 @@
 <?php 
 header('Content-Type: application/json');
-$searchString = "lord of the rings";
+
 $baseUrl = "https://openlibrary.org/search.json?";
+$query = http_build_query(array('q' => $_GET["query"]));
 
-$query = http_build_query(array('q' => $searchString));
+$data = json_decode(file_get_contents($baseUrl.$query));
 
-echo file_get_contents($baseUrl.$query);
+// function book_test($book) {
+//     return
+//     $book->title != "undefined" ||
+//     $book->author_name != "undefined" ||
+//     $book->cover_i != "undefined";
+// }
+
+// array_filter($data->docs, "book_test");
+
+echo json_encode($data->docs);
 ?>
